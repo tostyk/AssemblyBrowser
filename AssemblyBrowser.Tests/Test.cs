96a6eb9d547bbc48b3a10ac21d9ad2915efa763f
tostyk <ostyk47@gmail.com>
2022-11-15 13:31:35 +0300

    Update Tests
    Add tests

--- AssemblyBrowser.Tests/Test.cs
+++ AssemblyBrowser.Tests/Test.cs
@@ -1,19 +1,20 @@
+using AssemblyBrowser.Core;
 using AssemblyBrowser.Core.AssemblyClasses;
 
 namespace AssemblyBrowser.Tests
 {
     public class Tests
     {
-        Core.AssemblyBrowser assemblyBrowser = new Core.AssemblyBrowser();
+        readonly Core.AssemblyBrowser assemblyBrowser = new();
         AssemblyInformation ai;
         [SetUp]
         public void Setup()
         {
-            ai = assemblyBrowser.GetAssemblyInformation("LibForTesting.dll", 0);
+            ai = assemblyBrowser.GetAssemblyInformation("LibForTesting.dll", AssemblyBrowserFlags.OnlyDeclaredMembers);
         }
 
         [Test]
-        public void NamespacesNameAndCount()
+        public void Namespaces_NameAndCount()
         {
             Assert.That(ai, Is.Not.Null);
             Assert.That(ai.Namespaces, Is.Not.Null);
@@ -27,7 +28,7 @@ namespace AssemblyBrowser.Tests
         }
 
         [Test]
-        public void TypesNameAndCount()
+        public void Types_NameAndCount()
         {
             Namespace? n1 = ai.Namespaces.Find(o => o.Name == "Namespace1");
             Namespace? n2 = ai.Namespaces.Find(o => o.Name == "Namespace2");
@@ -52,9 +53,150 @@ namespace AssemblyBrowser.Tests
         }
 
         [Test]
-        public void FieldsNameAndCount()
+        public void Fields_NameAndCount()
         {
+            Namespace? n1 = ai.Namespaces.Find(o => o.Name == "Namespace1");
+            Namespace? n2 = ai.Namespaces.Find(o => o.Name == "Namespace2");
 
+            Core.AssemblyClasses.Type? class1 = n1?.Types.Find(o => o.Name == "Class1");
+            Core.AssemblyClasses.Type? staticClass = n1?.Types.Find(o => o.Name == "StaticClass");
+            Core.AssemblyClasses.Type? class2 = n2?.Types[0];
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1, Is.Not.Null);
+                Assert.That(staticClass, Is.Not.Null);
+                Assert.That(class2, Is.Not.Null);
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Fields, Has.Count.EqualTo(2));
+                Assert.That(staticClass?.Fields, Has.Count.EqualTo(0));
+                Assert.That(class2?.Fields, Has.Count.EqualTo(1));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class2?.Fields[0].Name, Is.EqualTo("Field1"));
+                Assert.That(class2?.Fields[0].Type, Is.EqualTo(typeof(string).Name));
+                Assert.That(class2?.Fields[0].AccessModifier, Is.EqualTo(AccessModifier.Public));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Fields[0].Name, Is.EqualTo("Field1"));
+                Assert.That(class1?.Fields[0].Type, Is.EqualTo(typeof(string).Name));
+                Assert.That(class1?.Fields[0].AccessModifier, Is.EqualTo(AccessModifier.Private));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Fields[1].Name, Is.EqualTo("Field2"));
+                Assert.That(class1?.Fields[1].Type, Is.EqualTo(typeof(int).Name));
+                Assert.That(class1?.Fields[1].AccessModifier, Is.EqualTo(AccessModifier.Public));
+            });
+        }
+        [Test]
+        public void Properties_NameAndCount()
+        {
+            Namespace? n1 = ai.Namespaces.Find(o => o.Name == "Namespace1");
+            Namespace? n2 = ai.Namespaces.Find(o => o.Name == "Namespace2");
+
+            Core.AssemblyClasses.Type? class1 = n1?.Types.Find(o => o.Name == "Class1");
+            Core.AssemblyClasses.Type? staticClass = n1?.Types.Find(o => o.Name == "StaticClass");
+            Core.AssemblyClasses.Type? class2 = n2?.Types[0];
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1, Is.Not.Null);
+                Assert.That(staticClass, Is.Not.Null);
+                Assert.That(class2, Is.Not.Null);
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Properties, Has.Count.EqualTo(2));
+                Assert.That(staticClass?.Properties, Has.Count.EqualTo(0));
+                Assert.That(class2?.Properties, Has.Count.EqualTo(1));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class2?.Properties[0].Name, Is.EqualTo("Property1"));
+                Assert.That(class2?.Properties[0].Type, Is.EqualTo(typeof(int).Name));
+                Assert.That(class2?.Properties[0].AccessModifier, Is.EqualTo(AccessModifier.Public));
+                Assert.That(class2?.Properties[0].GetterModifier, Is.EqualTo(AccessModifier.Private));
+                Assert.That(class2?.Properties[0].SetterModifier, Is.EqualTo(AccessModifier.Public));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Properties[0].Name, Is.EqualTo("Property1"));
+                Assert.That(class1?.Properties[0].Type, Is.EqualTo(typeof(double).Name));
+                Assert.That(class1?.Properties[0].AccessModifier, Is.EqualTo(AccessModifier.Internal));
+                Assert.That(class1?.Properties[0].GetterModifier, Is.EqualTo(AccessModifier.Internal));
+                Assert.That(class1?.Properties[0].SetterModifier, Is.EqualTo(AccessModifier.Private));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Properties[1].Name, Is.EqualTo("Property2"));
+                Assert.That(class1?.Properties[1].Type, Is.EqualTo("Class1"));
+                Assert.That(class1?.Properties[1].AccessModifier, Is.EqualTo(AccessModifier.Protected));
+                Assert.That(class1?.Properties[1].GetterModifier, Is.EqualTo(AccessModifier.Private));
+                Assert.That(class1?.Properties[1].SetterModifier, Is.EqualTo(AccessModifier.Protected));
+            });
+        }
+
+        [Test]
+        public void Methods_NameAndCount()
+        {
+            Namespace? n1 = ai.Namespaces.Find(o => o.Name == "Namespace1");
+
+            Core.AssemblyClasses.Type? class1 = n1?.Types.Find(o => o.Name == "Class1");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1, Is.Not.Null);
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Fields, Has.Count.EqualTo(2));
+            });
+            Assert.That(class1?.Methods, Has.Count.EqualTo(4));
+            Method? method = class1?.Methods.Find(o => o.Name == "Method1");
+            Assert.That(method, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(method.IsExtensionMethod, Is.EqualTo(false));
+                Assert.That(method.ReturnType, Is.EqualTo(typeof(void).Name));
+                Assert.That(method.AccessModifier, Is.EqualTo(AccessModifier.Public));
+                Assert.That(method.Parameters, Has.Length.EqualTo(2));
+                Assert.That(method.Parameters[0].Name, Is.EqualTo("param1"));
+                Assert.That(method.Parameters[0].Type, Is.EqualTo(typeof(string).Name));
+                Assert.That(method.Parameters[1].Name, Is.EqualTo("param2"));
+                Assert.That(method.Parameters[1].Type, Is.EqualTo(typeof(int).Name));
+            });
+        }
+
+        [Test]
+        public void ExtensionMethod()
+        {
+            Namespace? n1 = ai.Namespaces.Find(o => o.Name == "Namespace1");
+
+            Core.AssemblyClasses.Type? class1 = n1?.Types.Find(o => o.Name == "Class1");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1, Is.Not.Null);
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(class1?.Fields, Has.Count.EqualTo(2));
+            });
+            Assert.That(class1?.Methods, Has.Count.EqualTo(4));
+            Method? method = class1?.Methods.Find(o => o.Name == "GetTen");
+            Assert.That(method, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(method.IsExtensionMethod, Is.EqualTo(true));
+                Assert.That(method.ReturnType, Is.EqualTo(typeof(int).Name));
+                Assert.That(method.AccessModifier, Is.EqualTo(AccessModifier.Public));
+                Assert.That(method.Parameters, Has.Length.EqualTo(0));
+            });
         }
     }
 }
